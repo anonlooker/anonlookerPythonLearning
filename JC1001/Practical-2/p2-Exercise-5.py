d246fb4e997ec8d8e54f71cd6bbbbc70a1123b95
anonlooker <g8319228@outlook.com>
2025-10-27 15:40:22 +0800

    new file:   JC1001/Practical-2/p2-Exercise-5.py

---
+++ JC1001/Practical-2/p2-Exercise-5.py
@@ -0,0 +1,7 @@
+#5) Write a program that asks the user for the length of the side of a square and then
+#calculates the area and perimeter and returns these values to the user.
+side_length = float(input("Enter the length of the side of the square: "))
+area = side_length ** 2
+perimeter = 4 * side_length
+print("Area of the square: ", area)
+print("Perimeter of the square: ", perimeter)
