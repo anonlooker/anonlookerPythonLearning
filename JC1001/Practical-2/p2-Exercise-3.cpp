7d8459a793b425390269652ccae329b434531fa1
anonlooker <g8319228@outlook.com>
2025-10-31 12:20:48 +0800

    modified:   JC1001/Practical-2/p2-Exercise-3.cpp

--- JC1001/Practical-2/p2-Exercise-3.cpp
+++ JC1001/Practical-2/p2-Exercise-3.cpp
@@ -1,8 +1,8 @@
 /*3) Write a program which converts temperature in degrees Fahrenheit to degrees
-#Celsius. Your program should include in the output, the original value in
-#Fahrenheit that has been converted into degrees Celsius. (a simple way to
-#perform this calculation is to deduct 32 from the Fahrenheit temperature, multiply
-#by 5 and then divide by 9).*/
+Celsius. Your program should include in the output, the original value in
+Fahrenheit that has been converted into degrees Celsius. (a simple way to
+perform this calculation is to deduct 32 from the Fahrenheit temperature, multiply
+by 5 and then divide by 9).*/
 #include <iostream>
 using std::cin;
 using std::cout;
