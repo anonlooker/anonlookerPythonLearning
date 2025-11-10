74a351cee56b62583d3d7bd4a8ea6235817ac697
anonlooker <g8319228@outlook.com>
2025-11-10 14:56:57 +0800

    modified:   JC1001/Practical-4/p4-Exercise-1.py

--- JC1001/Practical-4/p4-Exercise-1.py
+++ JC1001/Practical-4/p4-Exercise-1.py
@@ -1,6 +1,8 @@
-sum=0
+#1)  Write a program that asks the user for 20 numbers. Once all the numbers have 
+#been entered, the sum of all the numbers should be displayed.
+sumOfNumber=0
 for i in range(20):
     num=int(input())
-    sum+=num
-print(num)
+    sumOfNumber+=num
+print(sumOfNumber)
 #p4-Exercise1.py
