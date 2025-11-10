8d0362f833045a35bb11c8673ff1788717fe8817
anonlooker <g8319228@outlook.com>
2025-11-10 15:17:55 +0800

    modified:   JC1001/Practical-4/p4-Exercise-5.py

--- JC1001/Practical-4/p4-Exercise-5.py
+++ JC1001/Practical-4/p4-Exercise-5.py
@@ -1,3 +1,6 @@
+#5)  A  weather  station  records  the  average  temperature  every  day  for  7  days.  Write  a 
+#program  that  allows  the  user  to input the  temperature  each  day for  7  days  and  then 
+#displays the average temperature for that week. 
 sumTemp=0
 for i in range(7):
     tempToday=float(input(f"Temperature on day {i+1}: "))
