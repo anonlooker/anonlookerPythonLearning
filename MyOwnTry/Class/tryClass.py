d7e0b4f0dfff865c879a252973c020faae5cef24
anonlooker <g8319228@outlook.com>
2025-12-02 08:40:56 +0800

    modified:   MyOwnTry/Class/tryClass.py

--- MyOwnTry/Class/tryClass.py
+++ MyOwnTry/Class/tryClass.py
@@ -1,10 +1,11 @@
 class object:
-    """Base class for all objects"""
+    """
+    Base class for all classes
+    """
     name=str()
     def printName(self):
         print(self.name)
 class Book(object):
-    """Class representing a book"""
     isbn=int()
     def printIsbn(self):
         print("ISBN of \""+ self.name + "\" is " + str(self.isbn))
